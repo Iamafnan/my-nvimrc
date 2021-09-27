c0f11bdea26bf28508833e3227acd5ee1e2d2ff3
Iamafnan <jackjhakas67@gmail.com>
2021-09-27 23:18:05 +0530

    Added customzations for Nvim-Tree.lua

--- plug-configs/nvim-tree.lua
+++ plug-configs/nvim-tree.lua
@@ -1,7 +1,6 @@
 vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', '.github'}
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_refresh_wait = 500
-
 require'nvim-tree'.setup{
         disable_netrw = true,
         hijack_netrw = true,
@@ -15,5 +14,31 @@ require'nvim-tree'.setup{
         enable = false,
         update_cwd = false,
   },
-               
-}
+        show_icons = {
+        git = 1,
+        folders = 1,
+        files = 1,
+        folder_arrows = 1,
+        tree_width = 30,
+    },
+    icons = {
+      default = "",
+      symlink = "",
+      git = {
+        unstaged = "",
+        staged = "S",
+        unmerged = "",
+        renamed = "➜",
+        deleted = "",
+        untracked = "U",
+        ignored = "◌",
+      },
+      folder = {
+        default = "",
+        open = "",
+        empty = "",
+        empty_open = "",
+        symlink = "",
+      },
+    },
+  }
