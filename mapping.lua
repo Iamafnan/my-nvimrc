ea6b327392131104343d59ca60aa648de4ba5e9a
Iamafnan <jackjhakas67@gmail.com>
2021-10-04 13:38:33 +0530

    added some silence to mappings

--- mapping.lua
+++ mapping.lua
@@ -8,24 +8,24 @@ map('', 'k', 'gk', { noremap = false })
 map('', 'j' , 'gj', { noremap = false })
 
 -- for buffers movement
-map('n', ',q', ':BufferClose<CR>', { noremap = true })
-map('n', ',l', ':BufferNext<CR>', { noremap = true })
+map('n', ',q', ':BufferClose<CR>', { noremap = true , silent = true})
+map('n', ',l', ':BufferNext<CR>', { noremap = true , silent = true })
 
 -- for split movement
-map('', '<C-h>', '<C-w>h', { noremap = false } )
-map('', '<C-j>', '<C-w>j', { noremap = false } )
-map('', '<C-k>', '<C-w>k', { noremap = false } )
-map('', '<C-l>', '<C-w>l', { noremap = false } )
+map('', '<C-h>', '<C-w>h', { noremap = false , silent = true } )
+map('', '<C-j>', '<C-w>j', { noremap = false , silent = true } )
+map('', '<C-k>', '<C-w>k', { noremap = false , silent = true } )
+map('', '<C-l>', '<C-w>l', { noremap = false , silent = true } )
 
 -- for commentary
-map('n', 'cm', ':Commentary<CR>', { noremap = true })
+map('n', 'cm', ':Commentary<CR>', { noremap = true , silent = true })
 
 -- for nvimtree
-map('n', 'nt', ':NvimTreeToggle<CR>', { noremap = true })
+map('n', 'nt', ':NvimTreeToggle<CR>', { noremap = true , silent = true })
 
 -- for quick rc sourcing
 map('n', 'rcs', ':so ~/.config/nvim/init.vim<CR>', { noremap = false })
 
 -- for telescope
-map('n', ',f', ':Telescope find_files<CR>', { noremap = true })
-map('n', ',,', ':Telescope live_grep<CR>', { noremap =  true })
+map('n', ',f', ':Telescope find_files<CR>', { noremap = true , silent = true })
+map('n', ',,', ':Telescope live_grep<CR>', { noremap =  true , silent = true})
