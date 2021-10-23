7f9dde71c48f4ea82ac9ad102e5eedefae95688c
Iamafnan <jackjhakas67@gmail.com>
2021-10-23 19:29:53 +0530

    removed formatting.lua, icons.lua, indentline.lua

--- lua/ui/formatting.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
-
-function M.setup()
-end
-
-return M
--- lua/ui/indentline.lua
+++
@@ -1,7 +0,0 @@
-
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    char = "|",
-    buftype_exclude = {''}
-}
