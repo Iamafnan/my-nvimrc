94dd2e3943bab39c3b46486108782806721f4717
Iamafnan <jackjhakas67@gmail.com>
2021-11-23 20:07:18 +0530

    Updated telescope configuration

--- lua/telescope-config/config.lua
+++ lua/telescope-config/config.lua
@@ -1,4 +1,6 @@
 local actions = require("telescope.actions")
+local sorters = require("telescope.sorters")
+local previewers = require("telescope.previewers")
 require("telescope").setup({
 	defaults = {
 		layout_config = {
@@ -16,9 +18,9 @@ require("telescope").setup({
 		selection_strategy = "reset",
 		sorting_strategy = "ascending",
 		layout_strategy = "horizontal",
-		file_sorter = require("telescope.sorters").get_fuzzy_file,
+		file_sorter = sorters.get_fuzzy_file,
 		file_ignore_patterns = { "node_modules/*", ".git/*" },
-		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+		generic_sorter = sorters.get_generic_fuzzy_sorter,
 		path_display = {},
 		winblend = 0,
 		border = {},
@@ -26,14 +28,16 @@ require("telescope").setup({
 		color_devicons = true,
 		use_less = true,
 		set_env = { ["COLORTERM"] = "truecolor" },
-		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-		buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+		file_previewer = previewers.vim_buffer_cat.new,
+		grep_previewer = previewers.vim_buffer_vimgrep.new,
+		qflist_previewer = previewers.vim_buffer_qflist.new,
+		buffer_previewer_maker = previewers.buffer_previewer_maker,
 		mappings = {
 			i = {
-				["<esc>"] = actions.close,
 				["<CR>"] = actions.select_default + actions.center,
+			},
+			n = {
+				["q"] = actions.close,
 			},
 		},
 	},
