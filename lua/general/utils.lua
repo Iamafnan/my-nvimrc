1d19db847edc3c9d3d11c030e416e438a5dabee0
Iamafnan <jackjhakas67@gmail.com>
2021-11-24 07:15:08 +0530

    Removed trash

--- lua/general/utils.lua
+++ lua/general/utils.lua
@@ -4,8 +4,4 @@ utils.set_keymap = function(mode, lhs, rhs)
 	vim.api.nvim_set_keymap(mode, lhs, rhs, { silent = true, noremap = true })
 end
 
--- utils.on_attach = function(client, bufnr)
---         require('')
--- end
-
 return utils
