e8c1fcfedf1a26e5cf360ae87bc1081b75223961
Iamafnan <jackjhakas67@gmail.com>
2021-11-28 16:47:57 +0530

    Removed healthcheck option from dashboard

--- lua/ui/dashboard.lua
+++ lua/ui/dashboard.lua
@@ -5,6 +5,5 @@ vim.g.dashboard_custom_section = {
 	c = { description = { " Find Files             " }, command = "Telescope find_files" },
 	d = { description = { " Projects               " }, command = "Telescope projects" },
 	e = { description = { " Update Plugins         " }, command = "PackerUpdate" },
-	f = { description = { " Health Check           " }, command = "checkhealth" },
 }
 vim.g.dashboard_custom_footer = {}
