6a23e1c28ccafb4d886c8e2d3dc624bb0d80b402
Iamafnan <jackjhakas67@gmail.com>
2021-11-28 22:37:28 +0530

    updated telescope config

--- lua/telescope-config/config.lua
+++ lua/telescope-config/config.lua
@@ -19,7 +19,7 @@ require("telescope").setup({
 		sorting_strategy = "ascending",
 		layout_strategy = "horizontal",
 		file_sorter = sorters.get_fuzzy_file,
-		file_ignore_patterns = { "node_modules/*", ".git/*" },
+		file_ignore_patterns = { "node_modules", ".git" },
 		generic_sorter = sorters.get_generic_fuzzy_sorter,
 		path_display = {},
 		winblend = 0,
