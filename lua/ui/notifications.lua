dbd66ee17556424d8a452eb786ead3e30ae19db2
Iamafnan <jackjhakas67@gmail.com>
2021-12-02 21:55:52 +0530

    Improved notifications

--- lua/ui/notifications.lua
+++ lua/ui/notifications.lua
@@ -1,14 +1,18 @@
 local notify = require("notify")
-local filetype = vim.bo.filetype
+
+-- Nofify Setup
+notify.setup({
+   stages = "fade"
+})
 
 local M = {}
 
 M.lspstarted = function(client)
-	notify("Language server:" .. client .. " is ready!", "INFO", { title = "Language Server" })
+	notify("Language server: " .. client .. " is ready!", "INFO", { title = "Language Server" })
 end
 
 M.sourced = function()
-	notify("Sucessfully sourced the file", "INFO", { title = "System Notifications" })
+	notify("Sucessfully sourced " .. vim.fn.expand("%:~:.") , "INFO", { title = "System Notifications" })
 end
 
 -- M.
