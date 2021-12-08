5b34f51d24c11e2230a04ff38ada049cc88de566
Iamafnan <jackjhakas67@gmail.com>
2021-12-09 00:37:26 +0530

    Added install.sh for installation

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -o nounset 
+set -o errexit
+
+installPacker() {
+	git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
+}
+
+clonerepo() {
+	echo "Cloning Repository"
+   git clone https://github.com/Iamafnan/my-nvimrc ~/.config/nvim
+}
+
+initConfig(){
+   echo "Installing Packages"
+   cd ~/.config/nvim
+   nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+}
+
+echo "Installing Afnan's Neovim Configuration"
+
+
+if [ -e "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" ]; then
+	echo 'Packer already installed'
+else
+	rm -rf ~/.local/share/nvim/site/pack/packer
+	installPacker
+fi
+
+if [ -e "$HOME/.config/nvim" ]; then
+   rm -rf ~/.config/nvim
+   clonerepo
+fi
+
+initConfig
