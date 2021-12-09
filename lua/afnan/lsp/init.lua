784fc0a2ef6aebf9a249569164cbae38c879cd7f
Iamafnan <jackjhakas67@gmail.com>
2021-12-09 14:29:33 +0530

    Added lsp-signature configuration

--- lua/afnan/lsp/init.lua
+++ lua/afnan/lsp/init.lua
@@ -1,3 +1,4 @@
 require("afnan.lsp.language-servers")
 require("afnan.lsp.handlers")
 require("afnan.lsp.renamer")
+require("afnan.lsp.signature")
---
+++ lua/afnan/lsp/signature.lua
@@ -0,0 +1,5 @@
+require("lsp_signature").setup({
+   hint_enable= false,
+   handler_opts = { border = "none" },
+   max_width = 80
+})
