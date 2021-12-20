ee3428b4bb2dd44e34a6e6f746f340198555c55d
Iamafnan <jackjhakas67@gmail.com>
2021-12-20 19:00:58 +0530

    Removed cssmodules_ls

--- lua/afnan/lsp/language-servers.lua
+++ lua/afnan/lsp/language-servers.lua
@@ -115,9 +115,6 @@ nvim_lsp.cssls.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
 })
-nvim_lsp.cssmodules_ls.setup({
-   on_attach = on_attach,
-   capabilities = capabilities,
 })
 nvim_lsp.html.setup({
 	on_attach = on_attach,
