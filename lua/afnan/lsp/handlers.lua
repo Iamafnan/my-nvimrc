13bed0ff1ca1d70e09cc29f8144d5a72314d04ec
Iamafnan <jackjhakas67@gmail.com>
2021-12-23 13:46:52 +0530

    Updates lsp diagnostics settings

--- lua/afnan/lsp/handlers.lua
+++ lua/afnan/lsp/handlers.lua
@@ -3,7 +3,7 @@ vim.diagnostic.config({
     virtual_text = false,
     float = {
         show_header = true,
-        source = 'always',
+        source = 'if_many',
         focusable = false,
     },
     update_in_insert = true,
