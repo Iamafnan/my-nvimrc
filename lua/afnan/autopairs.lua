dcc169be9c810e048d8657f5d3a86822cc95c0e0
Iamafnan <jackjhakas67@gmail.com>
2022-01-03 21:08:46 +0530

    Added some other config to nvim-autopairs

--- lua/afnan/autopairs.lua
+++ lua/afnan/autopairs.lua
@@ -5,6 +5,11 @@ local cmp = require("cmp")
 npairs.setup({
 	break_line_filetype = nil,
 	check_ts = true,
+	ts_config = {
+		lua = { "string" },
+		javascript = { "template_string" },
+		java = false, 
+	},
 })
 
 cmp.event:on("confirm_done", autopairs.on_confirm_done())
