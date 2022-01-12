d53155522061a40581e4d64fa311c3f31bacd69a
Iamafnan <jackjhakas67@gmail.com>
2022-01-12 20:19:54 +0530

    Improve: Floating Diagnostics UI

--- lua/afnan/lsp/handlers.lua
+++ lua/afnan/lsp/handlers.lua
@@ -1,10 +1,101 @@
 -- Diagnostics Setup
+local codes = {
+	no_matching_function = {
+		message = " Can't find a matching function",
+		"redundant-parameter",
+		"ovl_no_viable_function_in_call",
+	},
+	empty_block = {
+		message = " That shouldn't be empty here",
+		"empty-block",
+	},
+	missing_symbol = {
+		message = " Here should be a symbol",
+		"miss-symbol",
+	},
+	expected_semi_colon = {
+		message = " Remember the `;` or `,`",
+		"expected_semi_declaration",
+		"miss-sep-in-table",
+		"invalid_token_after_toplevel_declarator",
+	},
+	redefinition = {
+		message = " That variable was defined before",
+		"redefinition",
+		"redefined-local",
+	},
+	no_matching_variable = {
+		message = " Can't find that variable",
+		"undefined-global",
+		"reportUndefinedVariable",
+	},
+	trailing_whitespace = {
+		message = " Remove trailing whitespace",
+		"trailing-whitespace",
+		"trailing-space",
+	},
+	unused_variable = {
+		message = " Don't define variables you don't use",
+		"unused-local",
+	},
+	unused_function = {
+		message = " Don't define functions you don't use",
+		"unused-function",
+	},
+	useless_symbols = {
+		message = " Remove that useless symbols",
+		"unknown-symbol",
+	},
+	wrong_type = {
+		message = " Try to use the correct types",
+		"init_conversion_failed",
+	},
+	undeclared_variable = {
+		message = " Have you delcared that variable somewhere?",
+		"undeclared_var_use",
+	},
+	lowercase_global = {
+		message = " Should that be a global? (if so make it uppercase)",
+		"lowercase-global",
+	},
+}
+
 vim.diagnostic.config({
 	virtual_text = false,
 	float = {
-		show_header = true,
-		source = "if_many",
 		focusable = false,
+		border = "single",
+		scope = "cursor",
+		source = "if_many",
+		format = function(diagnostic)
+			local code = diagnostic.user_data.lsp.code
+			print("diagnostic:")
+			for _, table in pairs(codes) do
+				if vim.tbl_contains(table, code) then
+					return table.message
+				end
+			end
+			return diagnostic.message
+		end,
+		header = { "Cursor Diagnostics:", "DiagnosticHeader" },
+		pos = 1,
+		prefix = function(diagnostic, i, total)
+			local icon, highlight
+			if diagnostic.severity == 1 then
+				icon = ""
+				highlight = "DiagnosticError"
+			elseif diagnostic.severity == 2 then
+				icon = ""
+				highlight = "DiagnosticWarn"
+			elseif diagnostic.severity == 3 then
+				icon = ""
+				highlight = "DiagnosticInfo"
+			elseif diagnostic.severity == 4 then
+				icon = ""
+				highlight = "DiagnosticHint"
+			end
+			return i .. "/" .. total .. " " .. icon .. "  ", highlight
+		end,
 	},
 	update_in_insert = true,
 })
@@ -20,7 +111,7 @@ vim.api.nvim_exec(
 	[[
   augroup ShowDiagnosticFloat
     autocmd!
-    autocmd CursorHold * lua vim.diagnostic.open_float(0, {focusable = false, scope = 'line', source = 'always'})
+    autocmd CursorHold * lua vim.diagnostic.open_float(0, {focusable = false, scope = 'line', source = 'if_many'})
   augroup end
 ]],
 	false
