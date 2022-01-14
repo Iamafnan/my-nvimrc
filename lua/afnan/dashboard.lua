e8cbfa6d01f7ce55a0cd139dc0dd6ed5193559db
Iamafnan <jackjhakas67@gmail.com>
2022-01-14 23:31:37 +0530

    Added custom header for dashboard

--- lua/afnan/dashboard.lua
+++ lua/afnan/dashboard.lua
@@ -1,4 +1,21 @@
 vim.g.dashboard_default_executive = "telescope"
+vim.g.dashboard_custom_header = {
+	[[                                                     ]],
+	[[                                                     ]],
+	[[                                                     ]],
+	[[  ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓ ]],
+	[[  ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒ ]],
+	[[ ▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░ ]],
+	[[ ▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██  ]],
+	[[ ▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒ ]],
+	[[ ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░ ]],
+	[[ ░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░ ]],
+	[[    ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░    ]],
+	[[          ░    ░  ░    ░ ░        ░   ░         ░    ]],
+	[[                                 ░                   ]],
+	[[                                                     ]],
+	[[                                                     ]],
+}
 vim.g.dashboard_custom_section = {
 	a = { description = { " New File               " }, command = "DashboardNewFile" },
 	b = { description = { " Sessions               " }, command = "SessionManager load_session" },
