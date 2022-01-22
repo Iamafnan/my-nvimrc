9f1e2374635273e082941d42ab3c0121f4b7e08e
Iamafnan <jackjhakas67@gmail.com>
2022-01-22 09:55:23 +0530

    Added mapping for github-notifications.nvim

--- lua/afnan/mapping.lua
+++ lua/afnan/mapping.lua
@@ -63,7 +63,7 @@ local mappings = {
 		f = { ":Telescope find_files<CR>", "Find Files" },
 		r = { ":Telescope live_grep<CR>", "Grep" },
 		p = { ":Telescope projects<CR>", "Projects" },
-		n = { ":Telescope neoclip<CR>", "Neoclip" },
+		n = { ":Telescope ghn<CR>", "Github Notifications" },
 		h = { ":Telescope help_tags<CR>", "Help tags" },
 	},
 	r = {
