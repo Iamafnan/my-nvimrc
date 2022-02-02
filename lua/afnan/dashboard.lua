584b6d8a31b1d74672b980c442ded20d16ceece3
Iamafnan <jackjhakas67@gmail.com>
2022-02-03 00:27:55 +0530

    Improved Dashboard UI

--- lua/afnan/dashboard.lua
+++ lua/afnan/dashboard.lua
@@ -1,4 +1,5 @@
 vim.g.dashboard_default_executive = "telescope"
+vim.g.dashboard_disable_statusline = 1
 vim.g.dashboard_custom_header = {
 	[[                                                     ]],
 	[[                                                     ]],
@@ -17,8 +18,9 @@ vim.g.dashboard_custom_header = {
 	[[                                                     ]],
 }
 vim.g.dashboard_custom_section = {
-	a = { description = { " New File               " }, command = "DashboardNewFile" },
-	b = { description = { " Find Files             " }, command = "Telescope find_files" },
-	c = { description = { " Update Plugins         " }, command = "PackerUpdate" },
+	a = { description = { " New File   " }, command = "DashboardNewFile" },
+	b = { description = { " Find Files " }, command = "Telescope find_files" },
+	c = { description = { " History    " }, command = "DashboardFindHistory" },
+	d = { description = { " Colorscheme" }, command = "Telescope colorscheme" },
 }
 vim.g.dashboard_custom_footer = {}
