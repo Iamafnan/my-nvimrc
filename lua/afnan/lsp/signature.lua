3831cdc274b6f78587e25b3d6340b2c27aa4f81e
Iamafnan <jackjhakas67@gmail.com>
2022-02-23 21:33:49 +0530

    Add borders to signature popup

--- lua/afnan/lsp/signature.lua
+++ lua/afnan/lsp/signature.lua
@@ -1,6 +1,8 @@
+local border = { " ", " ", " ", " ", " ", " ", " ", " " }
+
 require("lsp_signature").setup({
-	hint_enable = true,
-	handler_opts = { border = "none" },
+	hint_enable = false,
+	handler_opts = { border = border },
 	max_width = 40,
 	max_height = 50,
 })
