625704c846fa04e5029b4f4a89e21340decb3e42
Iamafnan <jackjhakas67@gmail.com>
2022-03-17 10:43:54 +0530

    feat(notification): Add new highlights and new icons for notifications

--- lua/afnan/notify.lua
+++ lua/afnan/notify.lua
@@ -1,26 +1,43 @@
+-- highlights
+vim.cmd("hi NotifyBg guifg=#1f2335 guibg=#1f2335")
+vim.cmd("hi NotifyFg guifg=#a9b1d6 guibg=#1f2335")
+vim.cmd("hi NotifyIcon guifg=#7dcfff guibg=#1f2335")
+vim.cmd("hi NotifyTitle guifg=#7dcfff guibg=#1f2335 gui=bold")
+vim.cmd([[
+   highlight link NotifyERRORBorder NotifyBg
+   highlight link NotifyWARNBorder NotifyBg
+   highlight link NotifyINFOBorder NotifyBg
+   highlight link NotifyDEBUGBorder NotifyBg
+   highlight link NotifyTRACEBorder  NotifyBg 
+   highlight link NotifyERRORIcon    NotifyIcon
+   highlight link NotifyWARNIcon     NotifyIcon
+   highlight link NotifyINFOIcon     NotifyIcon
+   highlight link NotifyDEBUGIcon    NotifyIcon 
+   highlight link NotifyTRACEIcon    NotifyIcon
+   highlight link NotifyERRORTitle   NotifyTitle  
+   highlight link NotifyWARNTitle    NotifyTitle 
+   highlight link NotifyINFOTitle    NotifyTitle 
+   highlight link NotifyDEBUGTitle   NotifyTitle
+   highlight link NotifyTRACETitle   NotifyTitle
+   highlight link NotifyERRORBody NotifyFg
+   highlight link NotifyWARNBody NotifyFg
+   highlight link NotifyINFOBody NotifyFg
+   highlight link NotifyDEBUGBody NotifyFg
+   highlight link NotifyTRACEBody NotifyFg
+]])
+
 local notify = require("notify")
 local default = {
 	stages = "fade_in_slide_out",
 	render = "default",
 	timeout = 2000,
-	background_colour = function()
-		local group_bg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Normal")), "bg#")
-		if group_bg == "" or group_bg == "none" then
-			group_bg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Float")), "bg#")
-			if group_bg == "" or group_bg == "none" then
-				return "#000000"
-			end
-		end
-		return group_bg
-	end,
-
 	minimum_width = 10,
 	icons = {
 		ERROR = "",
 		WARN = "",
-		INFO = "",
-		DEBUG = "",
-		TRACE = "✎",
+		INFO = "",
+		DEBUG = "",
+		TRACE = "",
 	},
 }
 
